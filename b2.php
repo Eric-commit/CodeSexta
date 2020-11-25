00fcd7f4f25dc4509f1480c3b528ff9fbc64bfe8
Eric-commit <73726390+Eric-commit@users.noreply.github.com>
2020-11-25 19:31:25 -0300

    Update b2.php

--- b2.php
+++ b2.php
@@ -1,58 +1,71 @@
 <?php
+
 	session_start();
 ?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Carrinho</title>
+	<title>Compras</title>
+
+	
 </head>
 <body>
-	<h2>Carrinho</h2>
+	<h2>Carrinho de compras</h2>
 	<div class="carrinho-container">
 
 <?php
-	
-	$items = array(['imagem' => 'item1.png','preco'=>'200'],
-		['nome'=>'curso1','imagem' => 'item2.png','preco' => '250'],
-		['imagem' => 'item3.png','preco' => '230']);
+	$items = array(['nome'=>'Camise masc','imagem'=>'camisetabobmascw.jpg','preco'=>'20.00'],
+		['nome'=>'Camise masc','imagem'=>'camisetagnrc.jpg','preco'=>'25.00']);
 
-	foreach ($items as $key => $value) {
+	foreach ($items as $key => $value){
+
 
 ?>
 
-	<div class="produto">
-		<img src="<?php echo $value['imagem'] ?>" />
-		<a href="?Adicionar=<?php echo $key ?> ">Adicionar ao carrinho</a>
-	</div>
-
-<?php 
-	}
-?>
+<div class="produto">
+	<img src="<?php echo $value['imagem']?>"/>
+	<a href="?adicionar=<?php echo $key ?>">Adicionar ao carrinho</a>
 
 </div>
 
 <?php
+
+}
+
+?>
+	</div>
+
+<?php
 	if(isset($_GET['adicionar'])){
-		$idproduto = (int) $_GET['adcionar'];
+		$idproduto = (int) $_GET['adicionar'];
 		if(isset($items[$idproduto])){
-			if(isset($_session[$idproduto])){
-				$_session[$idproduto]['quantidade']++;
+			if(isset($_SESSION['carrinho'][$idproduto])){
+				$_SESSION['carrinho'][$idproduto]['quantidade']++;
 			}else{
-				$_session[$idproduto] *  array('quantidade'=>1,);
+
+				$_SESSION['carrinho'][$idproduto] = array('quantidade'=>1,'nome'=>$items[$idproduto]['nome'],'preco'=>$items[$idproduto]['preco']);
 			}
-
-
-
-			echo "O produto exite";
+			echo '<script> alert ("Item adicionado ao carrinho!")</script>';	
 
 		}else{
-			die ("o produto não exite");
+			die('voce não pode adicionar um item que não existe.');
 		}
 
 	}
 
+?>
+<h2 class='title'>Carrinho</h2>
+
+<?php
+foreach ($_SESSION['carrinho'] as $key => $value) {
+	
+	echo '<p>Item: '.$value['nome'].' | Quantidade: '.$value['quantidade'].'|Total: '.($value['quantidade']*$value['preco']).'</p>';
+}
 
 ?>
+
 
 </body>
 </html>
